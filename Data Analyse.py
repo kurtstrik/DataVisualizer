a4793043257b3e3ea8980d128408542b59f4cadd
Haarhaufen <37582434+Haarhaufen@users.noreply.github.com>
2024-08-26 01:02:09 +0200

    revamped upper plot and polished labels and legends

--- Data Analyse.py
+++ Data Analyse.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import requests, sys, csv, unicodedata, matplotlib
+import os
 from bs4 import BeautifulSoup
 
 r = requests.get('https://www.tuwien.at/tu-wien/ueber-die-tuw/zahlen-und-fakten#c9048')
@@ -91,10 +92,13 @@ edit = pd.concat([edit, rest])
 
 print(edit.to_string(index=False))
 females = edit['Frauen']
+females = females.iloc[::-1]#reverse the DataFrame for the hbar
 males = edit['Männer']
+males = males.iloc[::-1]#reverse the DataFrame for the hbar
 studies = edit['Studienrichtung']
 x_axis = np.arange(len(studies))
-shortnames = ['A', 'Inf', 'B', 'E', 'Mas', 'TP', 'WG', 'TC', 'TM', 'RR', 'WInf', 'V', 'BE', 'VG', 'Sonst']
+studies = studies.iloc[::-1]
+shortnames = ['A', 'Inf', 'B', 'E', 'Mas', 'TP', 'WIng', 'TC', 'TM', 'RR', 'WInf', 'V', 'BE', 'VG', 'Sonst']
 
 
 plot = matplotlib.pyplot
@@ -102,10 +106,38 @@ plot = matplotlib.pyplot
 fig, (ax1,ax2) = plot.subplots(2,1)
 
 #fig.set_figwidth(15)
-fig.set_size_inches(15,7)
-fig.tight_layout()
+fig.set_size_inches(16,7)
+#fig.tight_layout()
 
 
+
+
+#for i,j in zip(males, females):
+#ax1.barh(x_axis, [males, females],)
+x_axis2 = np.arange(0, len(studies)*2, step=2.0)
+
+ax1.barh(x_axis2 , males, 1.25, color = '#069AF3', label='Männer')
+ax1.barh(x_axis2 , females, 1.25, color = '#DC143C', label='Frauen', left=males)
+
+# TODO: set yticks!
+
+
+ax1.set_yticks(x_axis2, studies)
+ax1.set_xlabel("Studenten")
+ax1.grid(axis='x',linestyle='--')
+
+
+#xticks = np.linspace(0, 5500, 12) if finer grid spacing is required
+#ax1.set_xticks(xticks)
+
+
+ax1.legend()
+
+
+
+
+
+''' VERTICAL BAR CHARTS
 ax1.bar(x_axis - 0.2, females, 0.4, color = '#DC143C', label='Frauen')
 ax1.bar(x_axis + 0.2, males, 0.4, color = '#069AF3', label='Männer')
 ax1.set_xticks(x_axis, shortnames)
@@ -113,16 +145,24 @@ ax1.set_ylabel("Studenten")
 #ax1.xticks(x_axis, shortnames)
 #ax1.ylabel("Studenten")
 ax1.grid(axis='y',linestyle='--')
-ax1.legend()
+ax1.legend()'''
 
 ax1.set_title('Studierende TU Wien 06.01.2024')
 #ax1.title('Studierende TU Wien 06.01.2024')
-totals = edit['Studien gesamt'].sort_values(ascending=False)
+totals = edit['Studien gesamt'].sort_values()
 inc = 0
 
 for total in totals:
     maxgender = max(edit['Frauen'].iloc[inc], edit['Männer'].iloc[inc])
-    ax1.text(inc, total/2, total, ha='center', weight ='bold', rotation = 20) # total/2
+
+    men = males.iloc[inc]
+    women = females.iloc[inc]
+   
+    ax1.text(men/2, (inc*2)-0.7, men, ha='center', color='black')
+    
+    wwidth = (women/2) if inc != 0 else women
+    ax1.text(wwidth + (men), (inc*2)-0.7, women, ha='center', color='black')    
+    ax1.text(total + 150, (inc*2)-0.7, total, ha='center', weight ='bold') # total/2
     inc+=1
 
 
@@ -132,18 +172,26 @@ labels = edit['Studienrichtung']
 # https://stackoverflow.com/questions/23577505/how-to-avoid-overlapping-of-labels-autopct-in-a-pie-chart
 ratios = 100.*values/ values.sum()
 
-legendtext = ['{0} - {1:1.2f} %'.format(i,j) for i,j in zip(labels, ratios)]
-patches, texts = ax2.pie(values, labels=shortnames, radius = 1.2) # , autopct='%.3f'
-ax2.legend( legendtext,loc='right',bbox_to_anchor=(3.0, 0.6),fontsize=8)
+legendtext = ["{0} - {1:1.2f} %".format(i,j) for i,j in zip(labels, ratios)]
+
+inc2 = 0
+newtext = []
+for tex in legendtext:
+    boldtext = '$\\bf{'+shortnames[inc2]+'}$'
+    newtext.append(boldtext+' '+tex)
+    inc2 += 1
+
+patches, texts = ax2.pie(values, labels=shortnames,labeldistance= 1.2, radius = 1.0) # , autopct='%.3f'
+ax2.legend( newtext,loc='right',bbox_to_anchor=(2.25, 0.5),fontsize=8)
+
 
 # labels = ['{0} - {1:1.2f} %'.format(i,j) for i,j in zip(x, porcent)]
 
-plot.subplots_adjust(left=0.05, right=0.96, top =0.945)
+plot.subplots_adjust(left=0.195, right=0.993, top =0.945)
+ax2.set_position([-0.1 , 0.04, 0.5, 0.5])
+#plot.rcParams.update({'figure.autolayout': True})
 plot.show()
 #fig.show()
-
-# TODO: legend - shortnames
-
 
 second = soup.find(id="c9020")
 head2nd = second.find_all('th')
